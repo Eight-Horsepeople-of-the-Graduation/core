fe3ed01c8bca40c46e2a62575fa56eb6f0419b69
Relin404 <ahmadmashaal01@gmail.com>
2024-03-26 22:17:26 +0200

    chore(controllers): remove unnecessary try-catch blocks

--- src/controllers/lists.controller.ts
+++ src/controllers/lists.controller.ts
@@ -2,26 +2,14 @@ import { Request, Response } from "express";
 import * as listsService from "../services/lists.service";
 
 export const listAllLists = async (req: Request, res: Response) => {
-  try {
-    const lists = await listsService.getAllLists();
+  const lists = await listsService.getAllLists();
 
-    return res.send(lists);
-  } catch (error) {
-    console.error("Error listing all lists:", error);
-
-    return res.status(500).send("Internal Server Error");
-  }
+  return res.send(lists);
 };
 
 export const createList = async (req: Request, res: Response) => {
-  try {
-    const listData = req.body;
-    const list = await listsService.createList(listData);
+  const listData = req.body;
+  const list = await listsService.createList(listData);
 
-    return res.status(201).send(list);
-  } catch (error) {
-    console.error("Error creating list:", error);
-
-    return res.status(500).send("Internal Server Error");
-  }
+  return res.status(201).send(list);
 };
