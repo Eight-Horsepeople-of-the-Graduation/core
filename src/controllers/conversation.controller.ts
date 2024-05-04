85800b9e0f55f954ea23734e7f11c46c9db8221d
Relin404 <ahmadmashaal01@gmail.com>
2024-05-05 02:05:56 +0300

    refactor: Update conversation.controller.ts and conversation.repository.ts

--- src/controllers/conversation.controller.ts
+++ src/controllers/conversation.controller.ts
@@ -1,23 +1,24 @@
 import { Request, Response } from "express";
 import * as conversationService from "../services/conversation.service";
+import { HttpException } from "../exceptions/http.exception";
 
 export const getAllConversations = async (req: Request, res: Response) => {
   const conversations = await conversationService.getAllConversations();
-  return res.status(200).send(conversations);
+
+  return res.send(conversations);
 };
+
 export const getConversationById = async (req: Request, res: Response) => {
   const { id } = req.params;
-  if (!id) {
-    throw new Error("Get Conversation Error: Missing Conversation ID");
-  }
+
   const conversation = await conversationService.getConversationById(+id);
-  return res.status(200).send(conversation);
+
+  return res.send(conversation);
 };
+
 export const createConversation = async (req: Request, res: Response) => {
-  if (!req.body) {
-    throw new Error("Create Conversation Error: Missing Conversation Data");
-  }
   const conversationData = req.body;
+
   const createdConversation =
     await conversationService.createConversation(conversationData);
 
@@ -28,28 +29,32 @@ export const createConversation = async (req: Request, res: Response) => {
 
 export const createMessage = async (req: Request, res: Response) => {
   const { conversationId } = req.params;
-  if (!conversationId) {
-    throw new Error("Create Message Error: Missing Conversation ID");
-  }
-  if (!req.body) {
-    throw new Error("Create Message Error: Missing Message Data");
-  }
   const messageData = req.body;
+
   const createdMessage = await conversationService.createMessage(
     messageData,
     +conversationId
   );
+
   return res.status(201).send(createdMessage);
 };
+
 export const getMessagesByConversationId = async (
   req: Request,
   res: Response
 ) => {
   const { conversationId } = req.params;
-  if (!conversationId) {
-    throw new Error("Get Messages Error: Missing Conversation ID");
-  }
+
   const messages =
     await conversationService.getMessagesByConversationId(+conversationId);
-  return res.status(200).send(messages);
+
+  return res.send(messages);
+};
+
+export default {
+  getAllConversations,
+  getConversationById,
+  createConversation,
+  createMessage,
+  getMessagesByConversationId,
 };
