e110862fbf520edf76a55822065f8e1be8397c0d
Relin404 <ahmadmashaal01@gmail.com>
2024-05-07 04:21:22 +0300

    feat(api): implement general search functionality

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,6 +3,7 @@ import readingChallengesRouter from "../routes/reading-challenges.route";
 import usersRouter from "../routes/users.route";
 import booksRouter from "../routes/books.route";
 import bookshelfRouter from "../routes/bookshelves.route";
+import searchRouter from "../routes/search.route";
 
 /**
  * Registers all routes in the application
@@ -10,8 +11,9 @@ import bookshelfRouter from "../routes/bookshelves.route";
  * @returns void
  */
 export default (app: Express) => {
-  app.use("/books", booksRouter);
+  // app.use("/books", booksRouter);
   app.use("/bookshelves", bookshelfRouter);
   app.use("/reading-challenges", readingChallengesRouter);
+  app.use("/search", searchRouter);
   app.use("/users", usersRouter);
 };
