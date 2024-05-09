2fd8241e050e11f9b55a6b446ca1d63cca42bad7
Relin404 <ahmadmashaal01@gmail.com>
2024-05-09 04:39:27 +0300

    refactor: update books.route.ts to improve code organization and readability

--- src/routes/books.route.ts
+++ src/routes/books.route.ts
@@ -13,11 +13,17 @@ import asyncWrapper from "../utils/async-wrapper";
 const router = Router();
 
 router.post("/", asyncWrapper(createBook));
+
 router.get("/", asyncWrapper(getAllBooks));
+
 router.get("/:title", asyncWrapper(getAllBooksByTitle));
+
 router.delete("/:id", asyncWrapper(deleteBookById));
+
 router.put("/:id", asyncWrapper(updateBookById));
+
 router.get("/:id", asyncWrapper(getBookById));
+
 router.get("/book-of-user/:id", asyncWrapper(getBooksByUserId));
 
 export default router;
