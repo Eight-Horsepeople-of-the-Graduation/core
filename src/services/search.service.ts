059d92416db18ac54b7a54ca225220aa2b75f8c4
relin404 <ahmadmashaal01@gmail.com>
2024-05-14 03:23:03 +0300

    refactor: update search service to include bookshelves

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -1,17 +1,18 @@
 import { SearchQueryDto } from "@dtos";
-import booksService from "./books.service";
-import usersService from "./users.service";
+import booksService from "@services/books.service";
+import bookshelvesService from "@services/bookshelves.service";
+import usersService from "@services/users.service";
 
 export const search = async (query: SearchQueryDto) => {
-  const [books, /* bookshelves,*/ users] = await Promise.all([
+  const [books, bookshelves, users] = await Promise.all([
     booksService.getAllBooks(query),
-    // bookshelvesService.getAllBookshelves(query),
+    bookshelvesService.getAllBookshelves(query),
     usersService.getAllUsers(query),
   ]);
 
   return {
     books,
-    // bookshelves,
+    bookshelves,
     users,
   };
 };
