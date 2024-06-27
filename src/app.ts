bf9c99302d8e90747c41f6266bf1f7029e48a58d
relin404 <ahmadmashaal01@gmail.com>
2024-06-27 20:06:44 +0300

    Reapply "refactor: remove unnecessary migration files and add Swagger documentation for users API"
    This reverts commit a4e364bf58a5d00b50edddbbd7d4d5dd63f5715a.

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import config from "./config";
 import morgan from "morgan";
 import { errorHandlerMiddleware } from "./middleware/error-handler.middleware";
 import loadRouters from "@loaders/express";
-
+import * as swaggerJson from './swagger/swagger.json';
+import * as swaggerUI from 'swagger-ui-express';
 /**
  * Starts the server
  * @returns void
@@ -17,11 +18,12 @@ const startServer = async () => {
   app.use(express.urlencoded({ extended: true }));
 
   loadRouters(app);
-
+  app.use(['/docs', '/swagger'], swaggerUI.serve, swaggerUI.setup(swaggerJson));
   app.use(errorHandlerMiddleware);
 
   app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
+    console.log(`See Docs at http://localhost:${port}/docs`);
   });
 };
 
--- src/prisma/migrations/20240324220251_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Bookshelf" ALTER COLUMN "numOfBooks" DROP NOT NULL;
