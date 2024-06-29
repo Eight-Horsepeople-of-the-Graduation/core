b584b969e59ef89a593be3f1225d426f2263971b
Ahmad Tamer <alient2001@outlook.com>
2024-06-29 21:49:19 +0300

    Create User seeding script

--- src/prisma/seed/seeders/user.seeder.ts
+++ src/prisma/seed/seeders/user.seeder.ts
@@ -1,3 +1,40 @@
-export function seedUsers(num : number) {
-  throw new Error("Not implemented");
+import { faker } from "@faker-js/faker";
+import { Gender } from "@prisma/client";
+import prismaClient from "../../../utils/prisma";
+import {hashSync, genSaltSync} from "bcrypt";
+
+function createRandomUser() {
+  const gender = faker.helpers.arrayElement([Gender.male, Gender.female]);
+  const firstName = faker.person.firstName(gender);
+  const lastName = faker.person.lastName();
+  const email = faker.internet.email({ firstName, lastName });
+  const username = faker.internet.userName({ firstName, lastName });
+
+  return {
+    username,
+    email,
+    password: hashSync("password", genSaltSync(10)),
+    country: faker.location.country(),
+    gender,
+    birthDate: faker.date.birthdate(),
+    joinDate: faker.date.recent(),
+    profilePicture: faker.image.avatar(),
+    isAdmin: faker.helpers.arrayElement([true, false]),
+  };
+}
+
+export async function seedUsers(num: number) {
+  await prismaClient.user.deleteMany();
+  console.log("Deleted records in Users table...");
+
+  await prismaClient.$queryRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`;
+  console.log("Reset AUTO_INCREMENT in Users table...");
+
+  for (let i = 0; i < num; i++) {
+    await prismaClient.user.create({
+      data: createRandomUser(),
+    });
+  }
+
+  console.log("Added user data..");
 }
