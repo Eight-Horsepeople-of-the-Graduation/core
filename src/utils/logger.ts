ead9a3df7c51e997e6bc5649393e1df9fb3ae80c
relin404 <ahmadmashaal01@gmail.com>
2024-06-30 18:44:03 +0300

    refactor: change the log format

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,32 +1,66 @@
-import { createLogger, format, transports } from "winston";
-const { colorize, combine, prettyPrint, simple, timestamp } = format;
+import { createLogger, format, transports, addColors } from "winston";
+const { colorize, combine, printf, timestamp } = format;
 const { Console, File } = transports;
 
-const winstonFormat = combine(
-  timestamp({
-    format: "DD-MM-YYYY HH:mm:ss",
-  }),
-  prettyPrint()
-);
+// Define custom colors for different log levels
+addColors({
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  debug: "blue",
+  verbose: "cyan",
+  silly: "magenta",
+});
 
-const winstonTransports = [
-  new Console({
-    format: combine(winstonFormat, colorize({ all: true })),
-  }),
-  new File({
-    filename: "logs/error.log",
-    level: "error",
-    format: combine(winstonFormat, prettyPrint()),
-  }),
-  new File({
-    filename: "logs/all.log",
-    format: combine(winstonFormat, prettyPrint()),
-  }),
-];
+// Define a custom log format to match NestJS console output style
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}] ${message}`;
+});
 
+// Create a logger instance
 const logger = createLogger({
-  format: winstonFormat,
-  transports: winstonTransports,
+  level: "debug", // Set the default log level
+  format: combine(
+    timestamp({
+      format: "DD-MM-YYYY HH:mm:ss",
+    }),
+    logFormat // Apply the custom log format
+  ),
+  transports: [
+    new Console({
+      format: combine(
+        colorize({
+          all: true, // Apply color to all levels
+        }),
+        logFormat // Apply the custom log format with colors
+      ),
+    }),
+    new File({
+      filename: "logs/error.log",
+      level: "error", // Only log 'error' level messages to this file
+      format: combine(
+        timestamp({
+          format: "DD-MM-YYYY HH:mm:ss",
+        }),
+        format.printf(
+          ({ level, message, timestamp }) =>
+            `[${timestamp}] [${level}] ${message}`
+        )
+      ),
+    }),
+    new File({
+      filename: "logs/all.log",
+      format: combine(
+        timestamp({
+          format: "DD-MM-YYYY HH:mm:ss",
+        }),
+        format.printf(
+          ({ level, message, timestamp }) =>
+            `[${timestamp}] [${level}] ${message}`
+        )
+      ),
+    }),
+  ],
 });
 
 export default logger;
