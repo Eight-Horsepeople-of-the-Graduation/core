7d983e4c4735dc2101dfbc11a9549d596301f0fa
Ahmad Tamer <alient2001@outlook.com>
2024-06-30 08:33:53 +0300

    chore: Update bookshelf seeder to seed bookshelves with realistic data

--- src/prisma/seed/seeders/bookshelf.seeder.ts
+++ src/prisma/seed/seeders/bookshelf.seeder.ts
@@ -1,3 +1,33 @@
-export function seedBookshelves(num : number) {
-    throw new Error("Not implemented");
+import { faker } from "@faker-js/faker";
+import prismaClient from "../../../utils/prisma";
+import bookshelves from "../data/bookshelvesData.json";
+import { Privacy } from "@prisma/client";
+import { seedConfig } from "./config";
+export async function seedBookshelves(num: number) {
+  console.log(
+    "-----------------------------Seeding Bookshelves-----------------------------"
+  );
+  prismaClient.bookshelf.deleteMany();
+  console.log("Deleted records in Bookshelves table...");
+
+  prismaClient.$queryRaw`ALTER SEQUENCE "Bookshelf_id_seq" RESTART WITH 1`;
+  console.log("Reset AUTO_INCREMENT in Bookshelves table...");
+
+  // Add bookshelves
+  for (const bookshelf of bookshelves.data) {
+    const user = await prismaClient.user.findUniqueOrThrow({
+      where: { id: Math.floor(Math.random() * (seedConfig.userCount + 1)) },
+    });
+    prismaClient.bookshelf.create({
+      data: {
+        title: bookshelf.title,
+        description: bookshelf.description,
+        privacy: faker.helpers.arrayElement([Privacy.PRIVATE, Privacy.PUBLIC]),
+        createdAt: faker.date.soon({ days: 365, refDate: user.joinDate }),
+        userId: user.id,
+      },
+    });
+  }
+
+  console.log(`Added ${num} bookshelf data..`);
 }
