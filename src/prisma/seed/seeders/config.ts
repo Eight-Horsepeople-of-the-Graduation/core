38f18c185eef299486aa5245054ac2599786610b
Ahmad Tamer <alient2001@outlook.com>
2024-07-01 03:53:27 +0300

    chore: change seeding config values and add latest migration file

--- src/prisma/seed/seeders/config.ts
+++ src/prisma/seed/seeders/config.ts
@@ -1,10 +1,10 @@
 
 export const seedConfig = {
   userCount: 10,
-  bookshelfCount: 30,
-  bookCount: 12,
-  genreCount: 200,
-  authorCount: 200,
+  bookshelfCount: 15,
+  bookCount: 20,
+  genreCount: 1,
+  authorCount: 1,
   readingChallengeCount: 10,
-  reviewCount: 20,
+  reviewCount: 5,
 };
