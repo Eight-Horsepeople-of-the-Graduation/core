6f9fa649d4a1fbe9e314952f923fe4224f663d5c
Ahmad Tamer <alient2001@outlook.com>
2024-07-03 17:09:27 +0300

    fix: Add CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import * as swaggerUI from "swagger-ui-express";
 import { errorHandlerMiddleware } from "./middleware/error-handler.middleware";
 import loadRouters from "./loaders/express";
@@ -10,7 +11,7 @@ const CSS_URL =
 
 const app = express();
 
-
+app.use(cors())
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
