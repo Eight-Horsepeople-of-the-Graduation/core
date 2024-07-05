2a79c4b8dda4941815a6e837f197f3aadfa40dc0
Ahmad Tamer <alient2001@outlook.com>
2024-07-05 19:01:26 +0300

    refactor: Add return types for reviews Repository, Service, and Controller

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -16,3 +16,4 @@ export interface IUser {
 export type OptionalUser = IUser | null;
 export type UserWithoutPassword = Omit<IUser, "password">;
 export type BookshelfUser = Pick<IUser, "name" | "username" | "profilePicture">;
+export type ReviewUser = Pick<IUser, "name" | "username" | "profilePicture">;
