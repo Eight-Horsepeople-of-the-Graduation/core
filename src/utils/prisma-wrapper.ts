86794ef707e12da4095a4633de9cb2c316ba94ae
Ahmad Tamer <alient2001@outlook.com>
2024-07-05 10:23:13 +0300

    refactor: Remove unused imports in prisma-wrapper.ts

--- src/utils/prisma-wrapper.ts
+++ src/utils/prisma-wrapper.ts
@@ -1,13 +1,3 @@
-import { HttpStatus } from "@enums/http-status.enum";
-import { HttpException } from "@exceptions/http.exception";
-import {
-  PrismaClientKnownRequestError,
-  PrismaClientUnknownRequestError,
-  PrismaClientInitializationError,
-  PrismaClientRustPanicError,
-  PrismaClientValidationError,
-} from "@prisma/client/runtime/library";
-
 export async function prismaWrapper<T>(
   handler: (args: any) => Promise<T>,
   args: any
