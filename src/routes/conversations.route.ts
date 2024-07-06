1ee620fd9736040af7b1b4959ca3a3dd996ff330
Ahmad Tamer <alient2001@outlook.com>
2024-07-06 15:02:44 +0300

    Remove conversationId related routes and add get and deete conv By UserAndBook, update the chat route

--- src/routes/conversations.route.ts
+++ src/routes/conversations.route.ts
@@ -6,9 +6,6 @@ import asyncWrapper from "@utils/async-wrapper";
 
 const router: Router = Router();
 
-// router.get("/", asyncWrapper(conversationsController.getAllConversations));
-
-
 router.get(
   "/user/:userId/book/:bookId",
   asyncWrapper(conversationsController.getConversationByUserAndBook)
@@ -20,17 +17,15 @@ router.post(
   asyncWrapper(conversationsController.chat)
 );
 
-// router.delete(
-//   "/user/:userId/book/:bookId",
+router.delete(
+  "/user/:userId/book/:bookId",
 
-//   asyncWrapper(conversationsController.deleteConversation)
-// );
+  asyncWrapper(conversationsController.deleteConversation)
+);
 
 // router.delete(
 //   "/:messageId",
 //   asyncWrapper(conversationsController.deleteMessagePair)
 // );
-
-
 
 export default router;
