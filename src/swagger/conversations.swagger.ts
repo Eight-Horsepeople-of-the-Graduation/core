33acdd474c6ba78b39ab890bda78cd40baeee759
Ahmad Tamer <alient2001@outlook.com>
2024-07-06 15:03:16 +0300

    docs: update swagger

--- src/swagger/conversations.swagger.ts
+++ src/swagger/conversations.swagger.ts
@@ -1,7 +1,8 @@
-import { Body, Get, Path, Post, Route, Tags } from "tsoa";
+import { Body, Delete, Get, Path, Post, Route, Tags } from "tsoa";
 import { ChatDto, CreateConversationDto, CreateMessageDto } from "../dtos";
 import { IConversation } from "../interfaces/conversations.interface";
 import { IMessage } from "../interfaces/messages.interface";
+import { deleteConversation } from "@repositories/conversations.repository";
 
 @Route("conversations")
 @Tags("Conversations")
@@ -15,6 +16,12 @@ export class ConversationsSwagger {
 
   @Get("/user/:userId/book/:bookId")
   public getConversationByUserAndBook(
+    @Path() userId: number,
+    @Path() bookId: number
+  ): IConversation | any {}
+
+  @Delete("/user/:userId/book/:bookId")
+  public deleteConversation(
     @Path() userId: number,
     @Path() bookId: number
   ): IConversation | any {}
