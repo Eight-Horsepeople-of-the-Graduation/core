3361662bd35ce90d30ed788125d01c81b4dc8479
relin404 <ahmadmashaal01@gmail.com>
2024-07-07 19:03:21 +0300

    feat(auth): set user object in request in refresh token middleware

--- src/middleware/refresh-tokens.middleware.ts
+++ src/middleware/refresh-tokens.middleware.ts
@@ -19,6 +19,10 @@ export const refreshTokenMiddleware = async (
     const user = await usersService.getUserById(userId);
     if (!user || !user.refreshToken)
       return res.status(403).send("No refresh token found");
+
+    req.user = user;
+
+    next();
   } catch (err) {
     return res.status(403).send("Invalid refresh token");
   }
