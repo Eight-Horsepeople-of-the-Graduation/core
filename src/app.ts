3be589c04f0bc4c6aec10dfb5faf1c7217d4e739
Ahmad Tamer <alient2001@outlook.com>
2024-07-11 17:47:14 +0300

    chore: Add origin to cors

--- src/app.ts
+++ src/app.ts
@@ -20,8 +20,8 @@ declare module "express" {
 }
 const app = express();
 
-app.use(cors());
-app.use(helmet())
+app.use(cors({ origin: config.origin, credentials: true }));
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
