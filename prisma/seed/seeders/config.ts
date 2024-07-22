7bc719189b163d78cc9b3dc587bb65da0fc3cd37
Ahmad Tamer <alient2001@outlook.com>
2024-07-23 02:29:40 +0300

    tests: Add 2 tests to test the user's default bookshelves

--- prisma/seed/seeders/config.ts
+++ prisma/seed/seeders/config.ts
@@ -1,7 +1,7 @@
 export const seedConfig = {
   userCount: 15,
   bookshelfCount: 30,
-  bookCount: 100,
+  bookCount: 30,
   genreCount: 1,
   authorCount: 1,
   readingChallengeCount: 10,
--- src/tests/integration-tests/user.repository.test.ts
+++ src/tests/integration-tests/user.repository.test.ts
@@ -1,8 +1,11 @@
 import { SelectUserWithoutPassword } from "@common/interfaces/users.interface";
+import { getDefaultTitles } from "@common/utils/build-default-bookshelves";
 import prismaClient from "@common/utils/prisma";
 import { Gender } from "@modules/users/dtos/users.dto";
 import usersRepository from "@modules/users/users.repository";
-
+import { signUp } from "@modules/auth/auth.service";
+import { addBookToBookshelf } from "@modules/bookshelves/bookshelves.service";
+import { Format } from "@modules/books/dtos/books.dto";
 describe("User Repository Integration tests", () => {
   beforeAll(async () => {
     await prismaClient.user.create({
@@ -19,8 +22,17 @@ describe("User Repository Integration tests", () => {
 
   afterAll(async () => {
     await prismaClient.user.deleteMany();
+    await prismaClient.bookshelf.deleteMany();
+    await prismaClient.book.deleteMany();
+    await prismaClient.author.deleteMany();
+    await prismaClient.genre.deleteMany();
 
     await prismaClient.$queryRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`;
+    await prismaClient.$queryRaw`ALTER SEQUENCE "Bookshelf_id_seq" RESTART WITH 1`;
+    await prismaClient.$queryRaw`ALTER SEQUENCE "Book_id_seq" RESTART WITH 1`;
+    await prismaClient.$queryRaw`ALTER SEQUENCE "Author_id_seq" RESTART WITH 1`;
+    await prismaClient.$queryRaw`ALTER SEQUENCE "Genre_id_seq" RESTART WITH 1`;
+
     await prismaClient.$disconnect();
   });
 
@@ -123,5 +135,176 @@ describe("User Repository Integration tests", () => {
       },
     });
     expect(userWithHashedPassword).not.toBeNull;
+  });
+
+  it("should make sure the default bookshelves are created for a new user", async () => {
+    const user = await signUp({
+      username: "newuser",
+      email: "test@test.com",
+      name: "Ahmad Tamer Test",
+      password: "password",
+      country: "Egypt",
+      gender: Gender.MALE,
+      isAdmin: false,
+    });
+    const validUserId = user.user.id;
+    const bookshelves = await prismaClient.bookshelf.findMany({
+      where: {
+        userId: validUserId,
+      },
+    });
+
+    const bookshelvesTitles = bookshelves.map((bookshelf) =>
+      bookshelf.title?.toLowerCase()
+    );
+    expect(bookshelvesTitles).toEqual(getDefaultTitles());
+  });
+
+  it("should make sure the 3 default bookshelves are mutually exclusive", async () => {
+    const user = await signUp({
+      username: "newuser_2",
+      email: "test_2@test.com",
+      name: "Ahmad Tamer Test",
+      password: "password",
+      country: "Egypt",
+      gender: Gender.MALE,
+      isAdmin: false,
+    });
+    const newBook = {
+      title: "Test Book 2",
+      isbn: "08021247339",
+      description: "This is a test book",
+      publishDate: new Date("2021-01-01T00:00:00Z"),
+      format: Format.HARDCOVER,
+      language: "English",
+      country: "United States",
+      numOfPages: 100,
+      pdfLink: "https://test.com",
+      coverPicture: "https://test.com",
+      authors: { create: [{ name: "Test Author" }] },
+      genres: {
+        create: [{ title: "Test Genre", description: "Test description" }],
+      },
+    };
+    const book = await prismaClient.book.create({
+      data: newBook,
+    });
+
+    console.log(book);
+    const validUserId = user.user.id;
+    const bookshelves = await prismaClient.bookshelf.findMany({
+      where: {
+        userId: validUserId,
+      },
+      include: {
+        _count: {
+          select: {
+            books: true,
+          },
+        },
+      },
+    });
+
+    const bookshelvesTitles = bookshelves.map((bookshelf) => {
+      return { title: bookshelf.title?.toLowerCase(), id: bookshelf.id };
+    });
+
+    await addBookToBookshelf(bookshelvesTitles[0].id, [book.id]);
+    //Make sure the sum of all the default booksholeves is 1
+    expect(
+      (
+        await prismaClient.bookshelf.findMany({
+          where: { userId: validUserId },
+          include: {
+            _count: {
+              select: {
+                books: true,
+              },
+            },
+          },
+        })
+      ).reduce((acc, bookshelf) => acc + bookshelf._count.books, 0)
+    ).toBe(1);
+
+    //Make sure the target defalt bookshelf has this 1 book
+    expect(
+      (
+        await prismaClient.bookshelf.findUniqueOrThrow({
+          where: { id: bookshelvesTitles[0].id },
+          include: {
+            _count: {
+              select: {
+                books: true,
+              },
+            },
+          },
+        })
+      )._count.books
+    ).toBe(1);
+
+    await addBookToBookshelf(bookshelvesTitles[1].id, [book.id]);
+    //Make sure the sum of all the default booksholeves is 1
+    expect(
+      (
+        await prismaClient.bookshelf.findMany({
+          where: { userId: validUserId },
+          include: {
+            _count: {
+              select: {
+                books: true,
+              },
+            },
+          },
+        })
+      ).reduce((acc, bookshelf) => acc + bookshelf._count.books, 0)
+    ).toBe(1);
+
+    //Make sure the target default bookshelf has this 1 book
+    expect(
+      (
+        await prismaClient.bookshelf.findUniqueOrThrow({
+          where: { id: bookshelvesTitles[1].id },
+          include: {
+            _count: {
+              select: {
+                books: true,
+              },
+            },
+          },
+        })
+      )._count.books
+    ).toBe(1);
+
+    await addBookToBookshelf(bookshelvesTitles[2].id, [book.id]);
+    expect(
+      (
+        await prismaClient.bookshelf.findMany({
+          where: { userId: validUserId },
+          include: {
+            _count: {
+              select: {
+                books: true,
+              },
+            },
+          },
+        })
+      ).reduce((acc, bookshelf) => acc + bookshelf._count.books, 0)
+    ).toBe(1);
+
+    //Make sure the target default bookshelf has this 1 book
+    expect(
+      (
+        await prismaClient.bookshelf.findUniqueOrThrow({
+          where: { id: bookshelvesTitles[2].id },
+          include: {
+            _count: {
+              select: {
+                books: true,
+              },
+            },
+          },
+        })
+      )._count.books
+    ).toBe(1);
   });
 });
