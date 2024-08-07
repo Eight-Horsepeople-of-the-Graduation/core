eff59c9a3bef737439de7a3358becafd1bad5a61
Ahmad Tamer <alient2001@outlook.com>
2024-08-07 06:02:01 +0300

    refactor(user-seeding): Add a Test User by default

--- prisma/seed/seeders/user.seeder.ts
+++ prisma/seed/seeders/user.seeder.ts
@@ -7,31 +7,37 @@ export async function seedUsers(num: number) {
   );
 
   for (let i = 0; i < num; i++) {
-    signUp(createRandomUser());
+    signUp(createRandomUser(i == 0));
   }
 
   console.log(`Added ${num} users..`);
 }
 
-function createRandomUser() {
+function createRandomUser(test?: boolean) {
   const gender = faker.helpers.arrayElement([Gender.MALE, Gender.FEMALE]);
-  const firstName = faker.person.firstName(
-    gender.toLowerCase() as "male" | "female"
-  );
-  const lastName = faker.person.lastName();
-  const email = faker.internet.email({ firstName, lastName });
-  const username = faker.internet.userName({ firstName, lastName });
+  const firstName = test
+    ? "Test"
+    : faker.person.firstName(gender.toLowerCase() as "male" | "female");
+  const lastName = test ? "User" : faker.person.lastName();
+  const email = test
+    ? "test@test.com"
+    : faker.internet.email({ firstName, lastName });
+  const username = test
+    ? "TestUsername"
+    : faker.internet.userName({ firstName, lastName });
 
   return {
     name: `${firstName.toLowerCase()} ${lastName.toLowerCase()}`,
     username: username.toLowerCase(),
     email: email.toLowerCase(),
-    password: "password",
-    country: faker.location.country(),
+    password: test ? "Password$2001" : "password",
+    country: test ? "Egypt" : faker.location.country(),
     gender,
     birthDate: faker.date.birthdate(),
     joinDate: faker.date.recent({ days: 365, refDate: new Date() }),
-    profilePicture: faker.image.avatar(),
-    isAdmin: faker.helpers.arrayElement([true, false]),
+    profilePicture: test
+      ? "https://avatars.githubusercontent.com/u/33458793"
+      : faker.image.avatar(),
+    isAdmin: test ? true : faker.helpers.arrayElement([true, false]),
   };
 }
